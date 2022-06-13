bb258c40b55d7c0844173f54175fe16da07b7cd7
nick comer <nkcmr@users.noreply.github.com>
2022-06-12 23:54:41 -0700

    Upgrade tally to v4 (fixes uber-go/cadence-client#1157) (#1168)

--- test/integration_test.go
+++ test/integration_test.go
@@ -34,7 +34,7 @@ import (
 	"github.com/pborman/uuid"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
-	"github.com/uber-go/tally"
+	"github.com/uber-go/tally/v4"
 	"go.uber.org/goleak"
 	"go.uber.org/zap/zaptest"
 
